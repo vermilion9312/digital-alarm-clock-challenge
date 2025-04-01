2635910988deee95f570cc7ce58d80f5f9d7ed98
이주호 <83685855+vermilion9312@users.noreply.github.com>
2025-04-02 08:28:53 +0900

    Update behavior_tree.h

--- behavior-tree/challenge-1-1-control-led-using-button-input/User/Inc/behavior_tree.h
+++ behavior-tree/challenge-1-1-control-led-using-button-input/User/Inc/behavior_tree.h
@@ -8,14 +8,23 @@
 #ifndef INC_BEHAVIOR_TREE_H_
 #define INC_BEHAVIOR_TREE_H_
 
-#include "button.h"
-#include "led.h"
 
 typedef enum {
-	SUCCESS,
-	FAILURE
+	NODE_SUCCESS,
+	NODE_FAILURE
 } NodeState;
 
-typedef NodeState (* Node)(void);
+typedef NodeState (* Tick)(void*);
+typedef struct _LeafNode LeafNode;
+typedef struct _CompositeNode CompositeNode;
+
+struct _LeafNode {
+	Tick tick;
+};
+
+struct _CompositeNode {
+	Tick tick;
+	void** children
+};
 
 #endif /* INC_BEHAVIOR_TREE_H_ */
